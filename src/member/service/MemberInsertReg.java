ef081b1680a741f4b732636d5d981182fe69f234
kimhoyoun <hoyoun1309@gmail.com>
2022-06-20 22:58:19 +0900

    final

--- src/member/service/MemberInsertReg.java
+++ src/member/service/MemberInsertReg.java
@@ -13,9 +13,9 @@ public class MemberInsertReg implements MemberService{
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 			
-			String phonenum = request.getParameter("phonenum1")+
-								request.getParameter("phonenum2")+
-								request.getParameter("phonenum3");
+//			String phonenum = request.getParameter("phonenum1")+
+//								request.getParameter("phonenum2")+
+//								request.getParameter("phonenum3");
 			String email = request.getParameter("email1") + "@" + request.getParameter("email2");
 			int grade = 2;
 						
@@ -27,7 +27,7 @@ public class MemberInsertReg implements MemberService{
 			dto.setGender(request.getParameter("gender"));
 			dto.setEmail(email);
 			dto.setTelecom(request.getParameter("telecom"));
-			dto.setPhoneNum(phonenum);
+			dto.setPhoneNum(request.getParameter("phonenum"));
 			dto.setAddress(request.getParameter("address"));
 			dto.setDetailAddress(request.getParameter("detailAddress"));
 			dto.setGrade(grade);
